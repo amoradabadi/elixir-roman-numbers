4dc43070d24e05941c916be2c4825627b2696069
Amir Moradabadi <amoradabadi@8thlight.com>
2022-09-28 12:19:58 +0200

    convert 10 - refactored

--- lib/roman.ex
+++ lib/roman.ex
@@ -2,14 +2,10 @@ defmodule Roman do
   def convert(number) when number == 0, do: ""
 
   def convert(number) do
-    if number == 10 do
-      "X"
-    else
-      if number >= 5 do
-        "V" <> convert(number - 5)
-      else
-        "I" <> convert(number - 1)
-      end
+    cond do
+      number == 10 -> "X"
+      number >= 5 -> "V" <> convert(number - 5)
+      true -> "I" <> convert(number - 1)
     end
   end
 end
