3a104b90ff5a22b5ec2401c07d892aefa353ebf0
Amir Moradabadi <amoradabadi@8thlight.com>
2022-09-28 12:21:09 +0200

    convert 10 - refactored

--- test/roman_test.exs
+++ test/roman_test.exs
@@ -2,35 +2,17 @@ defmodule RomanTest do
   use ExUnit.Case
   doctest Roman
 
-  test "should convert number one" do
+  test "should convert units of one" do
     assert Roman.convert(1) == "I"
-  end
-
-  test "should convert number two" do
     assert Roman.convert(2) == "II"
-  end
-
-  test "should convert number three" do
     assert Roman.convert(3) == "III"
-  end
-
-  test "should convert number five" do
     assert Roman.convert(5) == "V"
-  end
-
-  test "should convert number six" do
     assert Roman.convert(6) == "VI"
-  end
-
-  test "should convert number seven" do
     assert Roman.convert(7) == "VII"
-  end
-
-  test "should convert number eight" do
     assert Roman.convert(8) == "VIII"
   end
 
-  test "should convert number ten" do
+  test "should convert units of ten" do
     assert Roman.convert(10) == "X"
   end
 
