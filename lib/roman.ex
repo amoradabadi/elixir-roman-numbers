8e5902d61f4b2de6b3b5d0adeede1735eeb7edc2
Amir Moradabadi <amoradabadi@8thlight.com>
2022-09-28 13:15:30 +0200

    using pipe

--- lib/roman.ex
+++ lib/roman.ex
@@ -2,18 +2,22 @@ defmodule Roman do
   def convert(number) when number == 0, do: ""
 
   def convert(number) do
-    cond do
-      number >= 1000 -> numeral(1000) <> convert(number - 1000)
-      number >= 500 -> numeral(500) <> convert(number - 500)
-      number >= 100 -> numeral(100) <> convert(number - 100)
-      number >= 50 -> numeral(50) <> convert(number - 50)
-      number >= 10 -> numeral(10) <> convert(number - 10)
-      number >= 5 -> numeral(5) <> convert(number - 5)
-      true -> numeral(1) <> convert(number - 1)
-    end
+    numeral_map()
+    |> Map.keys()
+    |> Enum.sort(:desc)
+    |> Enum.find(fn key -> number >= key end)
+    |> build(number)
+  end
+
+  defp build(l, number) do
+    numeral(l) <> convert(number - l)
   end
 
   defp numeral(number) do
+    numeral_map()[number]
+  end
+
+  def numeral_map() do
     %{
       1000 => "M",
       500 => "D",
@@ -22,6 +26,6 @@ defmodule Roman do
       10 => "X",
       5 => "V",
       1 => "I"
-    }[number]
+    }
   end
 end
